e2deda05aa8b3cc2a9d408fc98ce4da459f7e8a2
Sergey Chalyi <supersupergray@gmail.com>
2024-11-14 18:20:01 +0200

    Added dB schema

---
+++ base/schema.sql
@@ -0,0 +1,91 @@
+--
+-- File generated with SQLiteStudio v3.4.4 on ?? ??? 14 18:17:59 2024
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: auth_group
+CREATE TABLE "auth_group" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar(150) NOT NULL UNIQUE);
+
+-- Table: auth_group_permissions
+CREATE TABLE "auth_group_permissions" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "group_id" integer NOT NULL REFERENCES "auth_group" ("id") DEFERRABLE INITIALLY DEFERRED, "permission_id" integer NOT NULL REFERENCES "auth_permission" ("id") DEFERRABLE INITIALLY DEFERRED);
+
+-- Table: auth_permission
+CREATE TABLE "auth_permission" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "content_type_id" integer NOT NULL REFERENCES "django_content_type" ("id") DEFERRABLE INITIALLY DEFERRED, "codename" varchar(100) NOT NULL, "name" varchar(255) NOT NULL);
+
+-- Table: auth_user
+CREATE TABLE "auth_user" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "password" varchar(128) NOT NULL, "last_login" datetime NULL, "is_superuser" bool NOT NULL, "username" varchar(150) NOT NULL UNIQUE, "last_name" varchar(150) NOT NULL, "email" varchar(254) NOT NULL, "is_staff" bool NOT NULL, "is_active" bool NOT NULL, "date_joined" datetime NOT NULL, "first_name" varchar(150) NOT NULL);
+
+-- Table: auth_user_groups
+CREATE TABLE "auth_user_groups" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED, "group_id" integer NOT NULL REFERENCES "auth_group" ("id") DEFERRABLE INITIALLY DEFERRED);
+
+-- Table: auth_user_user_permissions
+CREATE TABLE "auth_user_user_permissions" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED, "permission_id" integer NOT NULL REFERENCES "auth_permission" ("id") DEFERRABLE INITIALLY DEFERRED);
+
+-- Table: comments_comment
+CREATE TABLE "comments_comment" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "user_name" varchar(255) NOT NULL, "email" varchar(254) NOT NULL, "text" text NOT NULL, "time_created" datetime NOT NULL, "attachment" varchar(100) NULL, "parent_comment_id" bigint NULL REFERENCES "comments_comment" ("id") DEFERRABLE INITIALLY DEFERRED);
+
+-- Table: django_admin_log
+CREATE TABLE "django_admin_log" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "object_id" text NULL, "object_repr" varchar(200) NOT NULL, "action_flag" smallint unsigned NOT NULL CHECK ("action_flag" >= 0), "change_message" text NOT NULL, "content_type_id" integer NULL REFERENCES "django_content_type" ("id") DEFERRABLE INITIALLY DEFERRED, "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED, "action_time" datetime NOT NULL);
+
+-- Table: django_content_type
+CREATE TABLE "django_content_type" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "app_label" varchar(100) NOT NULL, "model" varchar(100) NOT NULL);
+
+-- Table: django_migrations
+CREATE TABLE "django_migrations" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "app" varchar(255) NOT NULL, "name" varchar(255) NOT NULL, "applied" datetime NOT NULL);
+
+-- Table: django_session
+CREATE TABLE "django_session" ("session_key" varchar(40) NOT NULL PRIMARY KEY, "session_data" text NOT NULL, "expire_date" datetime NOT NULL);
+
+-- Index: auth_group_permissions_group_id_b120cbf9
+CREATE INDEX "auth_group_permissions_group_id_b120cbf9" ON "auth_group_permissions" ("group_id");
+
+-- Index: auth_group_permissions_group_id_permission_id_0cd325b0_uniq
+CREATE UNIQUE INDEX "auth_group_permissions_group_id_permission_id_0cd325b0_uniq" ON "auth_group_permissions" ("group_id", "permission_id");
+
+-- Index: auth_group_permissions_permission_id_84c5c92e
+CREATE INDEX "auth_group_permissions_permission_id_84c5c92e" ON "auth_group_permissions" ("permission_id");
+
+-- Index: auth_permission_content_type_id_2f476e4b
+CREATE INDEX "auth_permission_content_type_id_2f476e4b" ON "auth_permission" ("content_type_id");
+
+-- Index: auth_permission_content_type_id_codename_01ab375a_uniq
+CREATE UNIQUE INDEX "auth_permission_content_type_id_codename_01ab375a_uniq" ON "auth_permission" ("content_type_id", "codename");
+
+-- Index: auth_user_groups_group_id_97559544
+CREATE INDEX "auth_user_groups_group_id_97559544" ON "auth_user_groups" ("group_id");
+
+-- Index: auth_user_groups_user_id_6a12ed8b
+CREATE INDEX "auth_user_groups_user_id_6a12ed8b" ON "auth_user_groups" ("user_id");
+
+-- Index: auth_user_groups_user_id_group_id_94350c0c_uniq
+CREATE UNIQUE INDEX "auth_user_groups_user_id_group_id_94350c0c_uniq" ON "auth_user_groups" ("user_id", "group_id");
+
+-- Index: auth_user_user_permissions_permission_id_1fbb5f2c
+CREATE INDEX "auth_user_user_permissions_permission_id_1fbb5f2c" ON "auth_user_user_permissions" ("permission_id");
+
+-- Index: auth_user_user_permissions_user_id_a95ead1b
+CREATE INDEX "auth_user_user_permissions_user_id_a95ead1b" ON "auth_user_user_permissions" ("user_id");
+
+-- Index: auth_user_user_permissions_user_id_permission_id_14a6b632_uniq
+CREATE UNIQUE INDEX "auth_user_user_permissions_user_id_permission_id_14a6b632_uniq" ON "auth_user_user_permissions" ("user_id", "permission_id");
+
+-- Index: comments_comment_parent_comment_id_71289d4a
+CREATE INDEX "comments_comment_parent_comment_id_71289d4a" ON "comments_comment" ("parent_comment_id");
+
+-- Index: django_admin_log_content_type_id_c4bce8eb
+CREATE INDEX "django_admin_log_content_type_id_c4bce8eb" ON "django_admin_log" ("content_type_id");
+
+-- Index: django_admin_log_user_id_c564eba6
+CREATE INDEX "django_admin_log_user_id_c564eba6" ON "django_admin_log" ("user_id");
+
+-- Index: django_content_type_app_label_model_76bd3d3b_uniq
+CREATE UNIQUE INDEX "django_content_type_app_label_model_76bd3d3b_uniq" ON "django_content_type" ("app_label", "model");
+
+-- Index: django_session_expire_date_a5c62663
+CREATE INDEX "django_session_expire_date_a5c62663" ON "django_session" ("expire_date");
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
